7a7e9ba2d51d03333b4c3c826490c37dbab3a3dc
Pedro Barros <barr0607@algonquinlive.com>
2020-01-13 15:37:55 -0500

    Adds Index.html and Script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = 'Pedro Barros'
+console.log(`Hello, my name is ${name}.`)
+
+let number = 5
+
+let product = number * 2
+
+console.log(`The number ${number} multiply by 2 is ${number * 2}.`)
+
+console.log(`the number multiplied by 5 is ${product * 5}.`)
+
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue - 7}.`)
